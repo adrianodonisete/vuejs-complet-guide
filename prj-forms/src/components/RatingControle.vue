1efc501a39b185cdee7f138021633ba74dadfa5c
Adriano Machado <adriano@systax.com.br>
2024-01-16 18:06:58 -0300

    add prj-forms

---
+++ prj-forms/src/components/RatingControle.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      <button>Poor</button>
+    </li>
+    <li>
+      <button>Average</button>
+    </li>
+    <li>
+      <button>Great</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+</style>
